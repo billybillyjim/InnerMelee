5bc4d3242da42dc8f36ca2b8b72d7a5fbfe66e83
billybillyjim <thereallmwels@gmail.com>
2016-11-22 12:09:28 +0900

    Comments

--- ControlStick.cs
+++ ControlStick.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ControlStick : MonoBehaviour {
 
+//Sets the background image of the control stick map.
     public void setImage(Sprite s)
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = s;
